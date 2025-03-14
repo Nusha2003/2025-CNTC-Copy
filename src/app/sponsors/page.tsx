e4853b33b641990a63edeb57cff10bccf5aed22e
Anusha Madapura <amadapur@usc.edu>
2025-03-14 12:20:30 -0700

    added rice

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -4,6 +4,7 @@ import ntmc from '../../../public/sponsors/ntmc.png';
 import ucsdeng from '../../../public/sponsors/ucsd_eng.png';
 import ieee from '../../../public/sponsors/ieee.png'
 import urh from '../../../public/sponsors/urh.png'
+import rice from '../../../public/sponsors/rice.png'
 import fanfan from '../../../public/sponsors/fan_fan.png'
 
 export default function Members() {
@@ -11,6 +12,7 @@ export default function Members() {
         { src: ieee, alt: "IEEE EMBS", link: "https://www.embs.org/"},
         { src: ucsdeng, alt: "UCSD Engineering", link: "https://jacobsschool.ucsd.edu/" },
         { src: urh, alt: "UCSD URH", link: "https://ugresearch.ucsd.edu/" },
+        { src: rice, alt: "Rice Neuroengineering Initiative", link: "https://ugresearch.ucsd.edu/" },
         { src: ntmc, alt: "NTMC", link:"https://neurotechmicrocreds.com/"},
     ];
 
@@ -25,9 +27,10 @@ export default function Members() {
                 <h2 className="text-5xl font-semibold text-center mb-12">Our Sponsors</h2>
 
                 {/* 2x2 Grid Layout */}
-                <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-2 gap-y-8">
+                <div className="w-full max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-0 gap-y-12">
                     {sponsors.map((sponsor, index) => (
-                        <a key={index} href={sponsor.link} target="_blank" rel="noopener noreferrer" className="flex justify-center">
+                        <a key={index} href={sponsor.link} target="_blank" rel="noopener noreferrer" 
+                        className={`flex justify-center ${sponsors.length % 2 !== 0 && index === sponsors.length - 1 ? 'sm:col-span-2' : ''}`}>
                             {/* White rounded box with hover effect */}
                             <div className="bg-white p-6 rounded-xl shadow-lg flex justify-center items-center w-[400px] h-[250px] transition-transform transform hover:scale-105 hover:shadow-2xl">
                                 <Image src={sponsor.src} alt={sponsor.alt} className="h-auto w-auto max-h-[120px]" />
